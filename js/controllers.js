dbc38340bc1e85fff659fa5b5459bf7c68d52b81
acchuang <cc.alan@gmail.com>
2015-04-22 14:16:26 +0800

    reinitialized files

---
+++ js/controllers.js
@@ -0,0 +1,53 @@
+function TodoCrtl($scope) {
+  $scope.newItem = '';
+  $scope.todoList = [];
+  $scope.addItem = function(){
+    if(this.newItem){
+       this.todoList.push({label:this.newItem});
+       this.newItem = '';
+    }
+   
+  }
+}
+
+
+function TodoCrtlRemovable($scope) {
+  $scope.newItem = '';
+  $scope.todoList = [];
+  $scope.addItem = function(){
+    if(this.newItem){
+       this.todoList.push({label:this.newItem,isFinish:false});
+       this.newItem = '';
+    }
+  }
+
+  $scope.removeItem = function(item){
+      item.isFinish = true;
+  }
+}
+
+
+function TodoCrtlUpdate($scope) {
+  $scope.newItem = '';
+  $scope.todoList = [];
+  $scope.addItem = function(){
+    if(this.newItem){
+       this.todoList.push({label:this.newItem,isFinish:false});
+       this.newItem = '';
+    }
+  }
+
+  $scope.removeItem = function(item){
+      item.isFinish = true;
+  }
+
+  $scope.edit = function(item){
+      item.editing = true;
+  }
+
+  $scope.save = function(item){
+    delete item.editing;
+  }
+
+}
+
